357b4592587b28a989c0bae32a736f618d3c8ab1
Денис Жидков <denis.zhidkov01@gmail.com>
2022-07-14 14:59:51 +0300

    Finished impl at some point, TODO: time display, fix bug with blob image

--- src/app/cameras/cameras.service.ts
+++ src/app/cameras/cameras.service.ts
@@ -1,6 +1,47 @@
 class CamerasService {
-  getFrame() {
-    console.log('Called getFrame method');
+  private EMPTY_STRING: string = '\r\n\r\n';
+
+  async getFrame() {
+    const response = await fetch(
+      'https://textile.spbpu.com/http/?' +
+        new URLSearchParams({
+          r: 'get_frame',
+          src: 'camera1',
+        }).toString(),
+    );
+    const contentType = response.headers.get('content-type');
+    if ((contentType !== null || true) && contentType!.indexOf('boundary=') > -1) {
+      const boundary = contentType!.split('boundary=')[1];
+      const bodyData = await response.text();
+      const splittedBody = bodyData.split(boundary);
+      const meta = this.getMeta(splittedBody);
+      const image = this.getImage(splittedBody);
+      return {
+        meta,
+        image,
+      };
+    }
+    throw new Error('Произошла ошибка сети'); // ???
+  }
+
+  private getMeta(splittedBody: string[]): Record<string, any> {
+    const res: Record<string, any> = {};
+    for (let i = 1; i < splittedBody.length - 2; i++) {
+      // Делим по пустой строке
+      const splittedEntry = splittedBody[i].split(this.EMPTY_STRING);
+      // В конце лишний знак <">.
+      let name = splittedEntry[0].split('name="')[1];
+      name = name.substring(0, name.length - 1);
+      res[name] = splittedEntry[1].split('\r\n')[0];
+    }
+    return res;
+  }
+
+  private getImage(splittedBody: string[]): Blob {
+    const splittedEntryOfImage = splittedBody[splittedBody.length - 2].split(this.EMPTY_STRING);
+    // Набор символов "\r\n--" стоит в конце строки
+    const value = splittedEntryOfImage[1].split('\r\n--')[0];
+    return new Blob([value], { type: 'image/jpeg' });
   }
 }
 
--- src/view/components/player/player.component.tsx
+++ src/view/components/player/player.component.tsx
@@ -4,41 +4,73 @@ import styles from './player.style.scss';
 
 import { camerasService } from '~/app/cameras/cameras.service';
 
-export const Player: FC = () => {
-  let interval: NodeJS.Timer;
+type PlayerProps = {
+  playerId: number;
+};
+
+export const Player: FC<PlayerProps> = props => {
+  const META_KEYS = [
+    'sourceName',
+    'tag',
+    'frameId',
+    'frameTime',
+    'frameGrabTime',
+    'width',
+    'height',
+    'srcWidth',
+    'srcHeight',
+    'shutter',
+    'gain',
+    'temperature',
+  ];
   const [isMetaShown, setIsShowed] = useState<boolean>(true);
   const [isStopped, setIsStopped] = useState<boolean>(false);
+  let interval: NodeJS.Timer;
 
   useEffect(() => {
     if (isStopped) {
       clearInterval(interval);
     } else {
       interval = setInterval(() => {
-        playFrames();
+        void playFrames();
       }, 167);
     }
     return () => clearInterval(interval);
   }, [isStopped]);
 
   const showMetaHandler = () => {
-    isMetaShown ? console.log('Meta disabled') : console.log('Meta enabled');
     setIsShowed(!isMetaShown);
   };
 
   const pauseHandler = () => {
     setIsStopped(!isStopped);
-    isStopped ? console.log('Resume') : console.log('Stop');
   };
 
-  const downloadHandler = () => {
-    //TODO: implement me
-    console.log('Download');
+  const playFrames = async () => {
+    const frame = await camerasService.getFrame();
+    if (props.playerId !== null || undefined) {
+      setMeta(frame.meta);
+      setImage(frame.image);
+    } else {
+      throw Error('Неправильно заданный props');
+    } // ???
   };
 
-  const playFrames = () => {
-    //TODO: implement me
-    console.log('New frame');
-    camerasService.getFrame();
+  const setMeta = (meta: Record<string, any>) => {
+    for (const key of META_KEYS) {
+      document.getElementById(`${key}_${props.playerId}`)!.innerText = `${key}: ${meta[key]}`;
+    }
+  };
+
+  const setImage = (image: Blob) => {
+    const imageElement = document.getElementById(`frame_${props.playerId}`) as HTMLImageElement;
+    const downloadLinkElement = document.getElementById(
+      `frameLink_${props.playerId}`,
+    ) as HTMLLinkElement;
+    URL.revokeObjectURL(imageElement.src);
+    const imageURL = URL.createObjectURL(image);
+    imageElement.src = imageURL;
+    downloadLinkElement.href = imageURL;
   };
 
   return (
@@ -46,12 +78,29 @@ export const Player: FC = () => {
       <div className={styles.player__img_view}>
         <img
           className={`${styles.player__img_view} ${styles.img}`}
-          /*src= TODO: implement me */
-          alt="картинка с камеры"
+          alt="Картинка с камеры"
+          id={`frame_${props.playerId}`}
         />
         {isMetaShown ? (
           <span className={`${styles.player__img_view} ${styles.meta_inf}`}>
-            {/* TODO: implement me */} Мета-информация о кадре
+            <ul>
+              <li id={`sourceName_${props.playerId}`}></li>
+              <li id={`tag_${props.playerId}`}></li>
+              <li id={`frameId_${props.playerId}`}></li>
+              <li id={`frameTime_${props.playerId}`}></li>
+            </ul>
+            <ul>
+              <li id={`frameGrabTime_${props.playerId}`}></li>
+              <li id={`width_${props.playerId}`}></li>
+              <li id={`height_${props.playerId}`}></li>
+              <li id={`srcWidth_${props.playerId}`}></li>
+            </ul>
+            <ul>
+              <li id={`srcHeight_${props.playerId}`}></li>
+              <li id={`shutter_${props.playerId}`}></li>
+              <li id={`gain_${props.playerId}`}></li>
+              <li id={`temperature_${props.playerId}`}></li>
+            </ul>
           </span>
         ) : (
           ''
@@ -64,12 +113,13 @@ export const Player: FC = () => {
           src={isStopped ? '../ic_pause_button.svg' : '../ic_play_button.svg'}
           alt={isStopped ? 'Кнопка паузы' : 'Кнопка проигрывания'}
         />
-        <img
-          onClick={downloadHandler}
-          className={`${styles.player__btm_panel__button} ${styles.download}`}
-          src="../ic_download_button.svg"
-          alt="Кнопка скачать"
-        />
+        <div className={`${styles.player__btm_panel__button} ${styles.download}`}>
+          <img src="../ic_download_button.svg" alt="Скачать изображение" />
+          <a
+            className={`${styles.player__btm_panel__button} ${styles.download} ${styles.link}`}
+            id={`frameLink_${props.playerId}`}
+          />
+        </div>
         <img
           onClick={showMetaHandler}
           className={`${styles.player__btm_panel__button} ${styles.toggle_meta}`}
