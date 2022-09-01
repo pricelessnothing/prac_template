ca3edd00ddc783d03a74a2039afe69f4e171c8ba
Денис Жидков <denis.zhidkov01@gmail.com>
2022-09-01 16:52:43 +0300

    Fixed meta

--- src/view/pages/cameras/cameras.component.tsx
+++ src/view/pages/cameras/cameras.component.tsx
@@ -1,7 +1,6 @@
 import { Camera } from '../../components/camera/camera.component';
 
 import styles from './cameras.style.scss';
-import { Player } from '~/view/components/player/player.component';
 
 const AMOUNT_CAMERAS: number = 6;
 
@@ -18,7 +17,6 @@ export const Cameras = () => {
     <div className={styles.cameras}>
       <div className={styles.wrapper}>{getCameras(AMOUNT_CAMERAS)}</div>
       <button className={styles.cameras__btn} />
-      <Player playerId={1} />
     </div>
   );
 };
